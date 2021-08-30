3852b74784c688c10531b5c4240aa85395607a48
Towsif13 <36164854+Towsif13@users.noreply.github.com>
2021-08-30 12:01:56 +0600

    Add files via upload

---
+++ moving target/DQN_target/train.py
@@ -0,0 +1,109 @@
+import json
+from environment import Environment
+import torch
+from agent import Agent
+from environment import Environment
+import numpy as np
+from collections import deque
+import matplotlib.pyplot as plt
+import time
+
+env = Environment()
+
+agent = Agent(state_size=2, action_size=4, seed=3)
+
+n_episodes = 10_000
+max_t = 200
+eps_start = 1.0
+eps_end = 0.01
+eps_decay = 0.995  # 0.999936
+
+
+def dqn(n_episodes=n_episodes, max_t=max_t, eps_start=eps_start, eps_end=eps_end, eps_decay=eps_decay):
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start                    # initialize epsilon
+    max_score = 300.0
+    for i_episode in range(1, n_episodes+1):
+        state = env.reset()
+        score = 0
+        for t in range(max_t):
+            # env.render()
+            # time.sleep(0.5)
+
+            action = agent.act(state, eps)
+            next_state, reward, done = env.step(action)
+            agent.step(state, action, reward, next_state, done)
+            # env.render()
+            # time.sleep(1)
+            state = next_state
+            score += reward
+            if done:
+                break
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        eps = max(eps_end, eps_decay*eps)  # decrease epsilon
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(
+            i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(
+                i_episode, np.mean(scores_window)))
+
+        # if i_episode % 10_000 == 0:
+        #     torch.save(agent.qnetwork_local.state_dict(),
+        #                'eps'+str(i_episode) + 'checkpoint.pth')
+
+        if np.mean(scores_window) >= max_score:
+            max_score = np.mean(scores_window)
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(
+                i_episode-100, np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(),
+                       'checkpoint.pth')
+    return scores
+
+
+start_time = time.time()
+scores = dqn()
+end_time = time.time()
+
+scores_dqn_np = np.array(scores)
+np.savetxt("scores.txt", scores_dqn_np)
+
+
+def convert(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+
+    return "Execution time: %d hours : %02d minutes : %02d seconds" % (hour, minutes, seconds)
+
+
+n = end_time-start_time
+train_time = convert(n)
+print(train_time)
+
+train_info_dictionary = {'algorithm': 'DQN_moving_target', 'eps_start': eps_start, 'eps_end': eps_end,
+                         'eps_decay': eps_decay, 'episodes': n_episodes, 'train_time': train_time}
+
+train_info_file = open('train_info.json', 'w')
+json.dump(train_info_dictionary, train_info_file)
+train_info_file.close()
+
+
+def moving_average(a, n=10):
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+
+scores_ma_dqn = moving_average(scores, n=10)
+
+# plot the scores
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(len(scores_ma_dqn)), scores_ma_dqn)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
